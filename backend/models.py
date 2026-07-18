a54a308ba85379d9f2d51d4bebb91eb4cacd84da
matheuspereirasantos0703-art <matheuspereirasantos0703@gmail.com>
2026-07-17 21:36:31 -0300

    Arrumando o cadastro de clientes

--- backend/models.py
+++ backend/models.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
 from database import Base
 
 class User(Base):
@@ -8,3 +9,17 @@ class User(Base):
     name = Column(String, nullable=False)
     email = Column(String, unique=True, index=True, nullable=False)
     hashed_password = Column(String, nullable=False)
+
+    clients = relationship("Client", back_populates="owner", cascade="all, delete-orphan")
+
+
+class Client(Base):
+    __tablename__ = "clients"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False)
+    email = Column(String, nullable=True)
+    phone = Column(String, nullable=True)
+    owner_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+
+    owner = relationship("User", back_populates="clients")
